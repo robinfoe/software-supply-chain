b9ba8fbfde558a139f13338bde1abde401455271
robinfoe <robin@rfoe-a01.vmware.com>
2020-09-01 13:26:19 +0800

    fixes

--- jenkins/pipe-full.groovy
+++ jenkins/pipe-full.groovy
@@ -1,7 +1,6 @@
 
 def modules = [:]
 def jobVar = [:]
-
 def proceedToProduction = false
 
 
@@ -64,87 +63,86 @@ parameters {
     } //end stag
 
     
-    // stage('Stage - CI') {          
+    stage('Stage - CI') {          
       
-    //   steps {
-    //     script {
-    //       def task = build (
-    //                   job: 'pipe-ci', 
-    //                   parameters: [
-    //                     string(name: 'appName', value: "${params.appName}"),
-    //                     string(name: 'buildNumber', value: "${BUILD_NUMBER}"),
-    //                     string(name: 'gitURL', value: "${params.gitURL}"),
-    //                     string(name: 'gitBranch', value: "${params.gitBranch}"),
-    //                     string(name: 'gitAppFolder', value: "${params.gitAppFolder}"),
-    //                     string(name: 'mavenProxyFile', value: "${params.mavenProxyFile}"),
+      steps {
+        script {
+          def task = build (
+                      job: 'pipe-ci', 
+                      parameters: [
+                        string(name: 'appName', value: "${params.appName}"),
+                        string(name: 'buildNumber', value: "${BUILD_NUMBER}"),
+                        string(name: 'gitURL', value: "${params.gitURL}"),
+                        string(name: 'gitBranch', value: "${params.gitBranch}"),
+                        string(name: 'gitAppFolder', value: "${params.gitAppFolder}"),
+                        string(name: 'mavenProxyFile', value: "${params.mavenProxyFile}"),
                         
-    //                     booleanParam(name: 'performDependencyCheck', value: "${params.performDependencyCheck}"),
-    //                     booleanParam(name: 'performCodeQualityCheck', value: "${params.performCodeQualityCheck}"),
-    //                   ]
-    //                 )
+                        booleanParam(name: 'performDependencyCheck', value: "${params.performDependencyCheck}"),
+                        booleanParam(name: 'performCodeQualityCheck', value: "${params.performCodeQualityCheck}"),
+                      ]
+                    )
 
-    //       jobVar.appCoordinate = task.getBuildVariables().get('APP_COORDINATE')
-
-
-    //     }
-    //   }
-    // }  //end stage
+          jobVar.appCoordinate = task.getBuildVariables().get('APP_COORDINATE')
 
 
-    // stage('Stage - CB') {          
+        }
+      }
+    }  //end stage
+
+
+    stage('Stage - CB') {          
       
-    //   steps {
-    //     script {
+      steps {
+        script {
 
-    //       // echo jobVar.appCoordinate
-    //       def task = build (
-    //                   job: 'pipe-cb-dockerfile', 
-    //                   parameters: [
-    //                     string(name: 'appName', value: "${params.appName}"),
-    //                     string(name: 'imagePrefix', value: "${params.imagePrefix}"),
+          // echo jobVar.appCoordinate
+          def task = build (
+                      job: 'pipe-cb-dockerfile', 
+                      parameters: [
+                        string(name: 'appName', value: "${params.appName}"),
+                        string(name: 'imagePrefix', value: "${params.imagePrefix}"),
                         
-    //                     string(name: 'buildNumber', value: "${BUILD_NUMBER}"),
+                        string(name: 'buildNumber', value: "${BUILD_NUMBER}"),
                         
-    //                     string(name: 'gitURL', value: "${params.gitURL}"),
-    //                     string(name: 'gitBranch', value: "${params.gitBranch}"),
-    //                     string(name: 'gitAppFolder', value: "${params.gitAppFolder}"),
+                        string(name: 'gitURL', value: "${params.gitURL}"),
+                        string(name: 'gitBranch', value: "${params.gitBranch}"),
+                        string(name: 'gitAppFolder', value: "${params.gitAppFolder}"),
                         
-    //                     string(name: 'mavenProxyFile', value: "${params.mavenProxyFile}"),
-    //                     string(name: 'appCoordinate', value: jobVar.appCoordinate ),
+                        string(name: 'mavenProxyFile', value: "${params.mavenProxyFile}"),
+                        string(name: 'appCoordinate', value: jobVar.appCoordinate ),
                         
                   
-    //                   ]
-    //                 )
+                      ]
+                    )
 
-    //     }
-    //   }
-    // }  //end stage
+        }
+      }
+    }  //end stage
 
 
     stage('Stage - CD - SIT ') {
       steps {
         script {
 
-          // echo jobVar.appCoordinate
-          // def task = build (
-          //             job: 'pipe-cd', 
-          //             parameters: [
-          //               string(name: 'appName', value: params.appName),
-          //               string(name: 'imagePrefix', value: params.imagePrefix),
+          def task = build (
+                      job: 'pipe-cd', 
+                      parameters: [
+                        string(name: 'appName', value: params.appName),
+                        string(name: 'imagePrefix', value: params.imagePrefix),
 
-          //               string(name: 'appUrlSuffix', value: params.appUrlSuffix),
-          //               string(name: 'namespacePrefix', value: params.namespacePrefix),
-          //               string(name: 'deployEnvironment', value: "sit"),
-          //               string(name: 'kubeResourceFolder', value: params.kubeResourceFolder),
+                        string(name: 'appUrlSuffix', value: params.appUrlSuffix),
+                        string(name: 'namespacePrefix', value: params.namespacePrefix),
+                        string(name: 'deployEnvironment', value: "sit"),
+                        string(name: 'kubeResourceFolder', value: params.kubeResourceFolder),
                         
-          //               // string(name: 'buildNumber', value: "${BUILD_NUMBER}"),
-          //               string(name: 'buildNumber', value: "14"),
+                        // string(name: 'buildNumber', value: "${BUILD_NUMBER}"),
+                        string(name: 'buildNumber', value: "14"),
                         
-          //               string(name: 'gitURL', value: params.gitURL ),
-          //               string(name: 'gitBranch', value: params.gitBranch ),
-          //               string(name: 'gitAppFolder', value: params.gitAppFolder )
-          //             ]
-          //           )
+                        string(name: 'gitURL', value: params.gitURL ),
+                        string(name: 'gitBranch', value: params.gitBranch ),
+                        string(name: 'gitAppFolder', value: params.gitAppFolder )
+                      ]
+                    )
 
 
 
@@ -169,10 +167,6 @@ parameters {
       when { expression { proceedToProduction  } } 
       steps {
         script {
-
-          
-          
-          echo jobVar.appCoordinate
           def task = build (
                       job: 'pipe-cd', 
                       parameters: [
