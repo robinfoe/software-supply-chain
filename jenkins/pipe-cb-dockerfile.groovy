cad95dabaca46f55587e017cd9c9898a7df439a2
robinfoe <robin@rfoe-a01.vmware.com>
2020-10-06 09:17:14 +0800

    cleanup

--- jenkins/pipe-cb-dockerfile.groovy
+++ jenkins/pipe-cb-dockerfile.groovy
@@ -58,8 +58,8 @@ parameters {
     string(name: 'buildNumber', defaultValue: '67', description: 'Build Number, propagated from parent task')
 
     // GIT
-    string(name: 'gitURL', defaultValue: 'https://github.com/robinfoe/bookstore-ms.git', description: 'Git Clone URL')
-    string(name: 'gitBranch', defaultValue: 'master', description: 'git project for app')
+    string(name: 'gitURL', defaultValue: 'http://git.tanzu-no.de/ivy/bookstore-ms.git', description: 'Git Clone URL')
+    string(name: 'gitBranch', defaultValue: 'ivy-1.0', description: 'git project for app')
     string(name: 'gitAppFolder', defaultValue: 'book', description: 'Application Root Folder, leave blank pom.xml is in Root directory')
 
     string(name: 'mavenProxyFile', defaultValue: '/tmp/m2/ivy-settings.xml', description: 'Location to settings.xml')
